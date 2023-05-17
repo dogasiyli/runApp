050a709d9130e9bebdb9f495d5fd259d046be796
dogasiyli-work-comp <dogasiyli@gmail.com>
2023-05-17 13:30:39 +0300

    some additions

---
+++ screens/homeScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export function HomeScreen() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top }}>
+      <Text style={{ fontSize: 28 }}>Content is in safe area.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
